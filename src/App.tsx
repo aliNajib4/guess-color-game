2381df0def108dde1312494ff1baea5e020fa5dd
aliNajibHegazy <alinajibhegazy@gmail.com>
2024-06-06 16:24:22 +0300

    finish project

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,54 @@
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
+const createColor = () => {
+  const randomItems = "0123456789ABCDEF";
+  let color = "";
+  for (let i = 0; i < 6; i++) {
+    const randomLatter = `${randomItems[Math.floor(Math.random() * randomItems.length)]}`;
+    color += randomLatter;
+  }
+  return `#${color}`;
+};
+
 const App = () => {
+  const [mainColor, setMainColor] = useState("");
+  const correct = useRef<null | number>(null);
+  const [answer, setAnswer] = useState<null | boolean>(null);
+  const status = answer === null ? "idle" : answer ? "winner" : "loser";
+
+  const reset = () => {
+    setMainColor(createColor());
+    correct.current = Math.floor(Math.random() * 3);
+  };
+
+  const handleClick = (idx: number) => {
+    setAnswer(idx === correct.current ? true : false);
+    reset();
+  };
+
+  useEffect(() => {
+    setMainColor(createColor());
+    correct.current = Math.floor(Math.random() * 3);
+  }, []);
   return (
     <main>
-      <div className="color-box"></div>
+      <div className="color-box" style={{ backgroundColor: mainColor }}></div>
       <div className="btns">
-        <button className="btn">button</button>
-        <button className="btn">button</button>
-        <button className="btn">button</button>
+        {Array(3)
+          .fill(0)
+          .map((_, idx) => (
+            <button
+              onClick={() => handleClick(idx)}
+              key={idx}
+              className={`btn ${correct.current === idx ? "correct" : ""}`}
+            >
+              {correct.current === idx ? mainColor : createColor()}
+            </button>
+          ))}
+      </div>
+      <div className={`result`}>
+        status: <span className={status}>{status}</span>;
       </div>
     </main>
   );
